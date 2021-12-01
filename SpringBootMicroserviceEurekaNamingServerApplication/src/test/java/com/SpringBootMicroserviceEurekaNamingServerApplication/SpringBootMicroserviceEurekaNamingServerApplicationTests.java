d29b619b1b3ebf703f562d4da594f14e5464dec9
MohsinM <MohsinMohammadWork@users.noreply.github.com>
2021-12-01 11:23:37 -0500

    first commit.

---
+++ SpringBootMicroserviceEurekaNamingServerApplication/src/test/java/com/SpringBootMicroserviceEurekaNamingServerApplication/SpringBootMicroserviceEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBootMicroserviceEurekaNamingServerApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroserviceEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
